b137a84dc8344ca6e7ce13ecbf2ce246081159e2
sanphandinh <san.phandinh@outlook.com>
2024-07-24 06:24:59 +0700

    update workflow github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://sanphandinh.github.io',
+  base: 'personal-blog',
   integrations: [tailwind(), mdx()],
   collections: {
     projects: {
