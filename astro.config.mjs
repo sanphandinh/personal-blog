fed58a293993862dce7fdf0e188e38691cc0c829
sanphandinh <san.phandinh@outlook.com>
2024-07-24 21:54:55 +0700

    update side url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sanphandinh.github.io',
-  base: 'personal-blog',
   integrations: [tailwind(), mdx()],
   collections: {
     projects: {
